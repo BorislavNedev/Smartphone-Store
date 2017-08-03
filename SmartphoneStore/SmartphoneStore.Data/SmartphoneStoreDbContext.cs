2e43137daab01eaeb26d9907fb1539ddf96f43dc
Borislav Nedev <borislav.nedev@abv.bg>
2017-08-03 13:57:47 +0300

    Create Web,Models and Data projects.

---
+++ SmartphoneStore/SmartphoneStore.Data/SmartphoneStoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartphoneStore.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class SmartphoneStoreDbContext : IdentityDbContext<User>
+    {
+        public SmartphoneStoreDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SmartphoneStoreDbContext Create()
+        {
+            return new SmartphoneStoreDbContext();
+        }
+    }
+}
---
+++ SmartphoneStore/SmartphoneStore.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartphoneStore.Web.MvcApplication" Language="C#" %>
